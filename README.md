5d69e2aa18a172fb3bbebe3fe41053bc59474cbb
MoartMedia <alex789189@gmail.com>
2020-12-11 20:48:30 +0900

    inital commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# DISCORD SLASH COMMAND DEMO
+
+## Installation
+
+### Clone this repository
+
+> - Using Git CLI  
+>
+>
+>   git clone <https://github.com/MoartMedia/Discord-Integration-demo.git>  
+>   cd ./Discord-Integration-demo  
+>  
+> - Or Download This Repository  
+
+### Install Node.js
+
+> [Download For Windows / MacOS](https://nodejs.org/en/download/)
+> Download For Linux Using apt
+>
+> ```bash
+> apt-get update
+> apt-get install nodejs -y
+> ```
+
+### Setup Dependencies
+
+```bash
+npm install
+```  
+
+### Start Application
+
+```bash
+// Before You start Application, Edit ./config.json  
+
+node .
+```  
---
+++ index.js
@@ -0,0 +1,66 @@
+const config = require('./config.json');
+const {Client} = require('discord.js');
+const client = new Client();
+
+client.login(config.token);
+client.on('ready',()=>{
+    console.log('준비가 되었다.');
+});
+
+client.on('message',(msg)=>{
+    const text = msg.content;
+    if(text.startsWith('!커맨드 등록')){
+        registerSlashCommands(msg.guild);
+        msg.reply('Slash 커맨드 등록이 완료되었습니다.')
+    }
+});
+
+client.on('raw',(evt)=>{
+if(evt.t !== 'INTERACTION_CREATE') return;
+const {d: data} = evt;
+
+if(data.type !== 2) return;
+const CommandData = data.data;
+
+    if(CommandData.name === 'hello'){
+        let TargetUser;
+        if(CommandData.options) TargetUser = CommandData.options.find(element => element.name == '언급할사람');
+        const channel = client.channels.cache.get(data.channel_id);
+
+        if(TargetUser){
+            const user = client.users.cache.get(TargetUser.value);
+            channel.send(`${user}님 안녕하세요!`,{allowedMentions:{parse:[]}});
+
+        }else{
+            channel.send('안녕하세요!',{allowedMentions:{parse:[]}});
+        }
+    }
+})
+
+function registerSlashCommands(guild){
+    const data = {}
+    data.name = "hello"
+    data.description = "안녕 세상을 봇이 표현을 해줄 거에요!"
+    data.options = new Array();
+    
+    const option = {};
+    option.name = "언급할사람"
+    option.description = "환영할 사람을 격하게 환영합니다."
+    /*
+     * type list:
+     * 1 = SubCommand
+     * 2 = SubCommandGroup
+     * 3 = String
+     * 4 = Integer
+     * 5 = Boolean
+     * 6 = User
+     * 7 = Channel
+     * 8 = Role
+    */
+    option.type = 6 // 6 = User
+    option.required = false
+
+    data.options.push(option);
+
+    client.api.applications(client.user.id).guilds(guild.id).commands().post({data});
+}
