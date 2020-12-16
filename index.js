f4caf72e9ba89303a78b6fb3155694506863dd70
모메MoMe <partio2@naver.com>
2020-12-16 21:05:25 +0900

    International ver

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const client = new Client();
 
 client.login(config.token);
 client.on('ready',()=>{
-    console.log('준비가 되었다.');
+    console.log('Ready');
 });
 
 client.on('message',(msg)=>{
     const text = msg.content;
-    if(text.startsWith('!커맨드 등록')){
+    if(text.startsWith('!register')){
         registerSlashCommands(msg.guild);
-        msg.reply('Slash 커맨드 등록이 완료되었습니다.')
+        msg.reply('Slash command register successful')
     }
 });
 
@@ -24,15 +24,15 @@ const CommandData = data.data;
 
     if(CommandData.name === 'hello'){
         let TargetUser;
-        if(CommandData.options) TargetUser = CommandData.options.find(element => element.name == '언급할사람');
+        if(CommandData.options) TargetUser = CommandData.options.find(element => element.name == 'mention');
         const channel = client.channels.cache.get(data.channel_id);
 
         if(TargetUser){
             const user = client.users.cache.get(TargetUser.value);
-            callback(data,`${user}님 안녕하세요!`);
+            callback(data,`${user} hello!`);
 
         }else{
-            callback(data,`안녕하세요!`);
+            callback(data,`hello!`);
         }
     }
 })
@@ -40,12 +40,12 @@ const CommandData = data.data;
 function registerSlashCommands(guild){
     const data = {}
     data.name = "hello"
-    data.description = "안녕 세상을 봇이 표현을 해줄 거에요!"
+    data.description = "say hello"
     data.options = new Array();
     
     const option = {};
-    option.name = "언급할사람"
-    option.description = "환영할 사람을 격하게 환영합니다."
+    option.name = "mention"
+    option.description = "say hello to mention user"
     /*
      * type list:
      * 1 = SubCommand
@@ -64,7 +64,7 @@ function registerSlashCommands(guild){
 
     client.api.applications(client.user.id).guilds(guild.id).commands().post({data});
 }
-function callback(oridata,message){
+function callback(eventdata,message){
    const data = {
         "type": 4,
         "data": {
@@ -74,5 +74,5 @@ function callback(oridata,message){
             "allowed_mentions": []
         }
     }
-    client.api.interactions(oridata.id)[oridata.token].callback().post({data});
+    client.api.interactions(eventdata.id)[eventdata.token].callback().post({data});
 }
